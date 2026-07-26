b980004f7d91f3b92a1fa8fe6397bf80a73e0335
Javier Mansilla Garzon <jmansilla@mansillayasoc.com.ar>
2026-07-25 22:17:03 -0300

    Corregir error de tipos en push.ts

--- lib/push.ts
+++ lib/push.ts
@@ -23,7 +23,7 @@ export async function subscribeToPush(): Promise<PushSubscription | null> {
 
   return registration.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicKey),
+    applicationServerKey: urlBase64ToUint8Array(publicKey) as BufferSource,
   });
 }
 
